cd2edaab44d77b6159120bc4c64dc7ce078a0fdc
Rayyan365 <rayanabdallah09@gamil.com>
2023-08-19 00:23:50 -0700

    count-characters assignment done

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // Good Luck 💪🏾
+countString:"fikrcamp bootcamp"
+function countString(str, letter) {
+
+    const re = new RegExp(letter, '');
+
+    const count = str.match(re).length;
+
+    return count;
+}
+
+const string = prompt('fikrcamp bootcamp');
+const letterToCheck = prompt('o');
+
+const result = countString(string, letterToCheck);
+
+console.log(result);
